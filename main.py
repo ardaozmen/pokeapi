c9cc416fc078959f8541905cfcee8e8ddecc4ed5
Arda Cem Özmen <ardaozmen3@gmail.com>
2022-09-09 13:40:54 +0300

    Some console prints added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.poke import Pokemon
 from utils.forward import Forward
+from helper.text_helper import TextHelper
 
 if __name__ == "__main__":
     # TODO ArgParser will be add.
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     Poke.html_2_pdf()
     sender = Forward()
     sender.send()
+    print(TextHelper.EMAIL_SENT_SUCCESSFULLY)
